27ba26a7c71b04b95e2a818720d684ad10996449
Henry Woody <github@henrywoody.com>
2018-05-25 18:38:47 -0700

    adds env variable to config

--- config.js
+++ config.js
@@ -2,12 +2,14 @@ const config = {};
 
 // Dev
 config.dev = {
+	env: 'dev',
 	dbHost: process.env.DB_HOST_DEV,
 	appSecret: process.env.APP_SECRET
 };
 
 // Test
 config.test = {
+	env: 'test',
 	dbHost: process.env.DB_HOST_TEST,
 	appSecret: process.env.APP_SECRET
 };
