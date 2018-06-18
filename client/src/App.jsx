2cd83a0e3773c607eacbcc8372cf4ea133563d1d
henrywoody <github@henrywoody.com>
2018-06-18 10:07:44 -0700

    adds React Router and header/nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Components/Header.jsx';
+import Home from './Pages/Home.jsx';
+import Wardrobe from './Pages/Wardrobe.jsx';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			user: {
+				_id: "5b269bbc16b19be6a4203974",
+				username: "test",
+				token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1YjI2OWJiYzE2YjE5YmU2YTQyMDM5NzQiLCJpYXQiOjE1MjkyNTY4OTJ9.Y4OZW6nH8L_6CRFSdhltzrpFkshPyRoWIjfbaovUIBI"
+			}
+		}
+	}
+
 	render() {
+		const { user } = this.state;
+
 		return (
 			<div className="App">
+				<Header links={ [
+						{name: 'Home', exact: true, href: '/'},
+						{name: 'Wardrobe', exact: false, href: '/wardrobe'}
+				] }/>
+
+				<Switch>
+					<Route exact path='/' render={ () => <Home user={ user }/> }/>
+					<Route path='/wardrobe' render={ () => <Wardrobe user={ user }/> }/>
+				</Switch>
 			</div>
 		);
 	}
 }
-
-export default App;
