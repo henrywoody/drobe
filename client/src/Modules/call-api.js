26c104ecc0c9f75deec6594ff28e94d65e7e2163
henrywoody <github@henrywoody.com>
2018-06-18 10:07:28 -0700

    adds call-api module for client

---
+++ client/src/Modules/call-api.js
@@ -0,0 +1,24 @@
+import queryString from 'query-string';
+
+export default async function callAPI(url, query, token, method, body) {
+	let fullUrl = `/api/v1/${url}`;
+
+	if (query)
+		fullUrl += `?${queryString.stringify(query)}`;
+
+	const additionalInfo = {
+		method: method,
+		headers: {
+			'Authorization': `JWT ${token}`
+		}
+	};
+
+	if (body) {
+		additionalInfo.headers['Content-Type'] = 'application/json';
+		additionalInfo.body = JSON.stringify(body);
+	}
+
+	const result = await fetch(fullUrl, additionalInfo);
+
+	return result.json();
+}
