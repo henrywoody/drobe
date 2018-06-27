ea19e22effab76e169c98038738a1b73451d0fbf
henrywoody <github@henrywoody.com>
2018-06-27 15:40:38 -0700

    adds server 'users/logout' call to client-side logout functionality

--- client/src/Pages/Logout.jsx
+++ client/src/Pages/Logout.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default class Logout extends Component {
 	componentWillMount() {
 		const { logUserOut } = this.props;
+		fetch('/users/logout');
 		logUserOut();
 	}
 
