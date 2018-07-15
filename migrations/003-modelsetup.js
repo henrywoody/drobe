a8f9e0cebbf5f103081f2082bc708f8e286f7c5a
henrywoody <github@henrywoody.com>
2018-07-15 13:33:03 -0700

    adds owner_id fk field to each article table, and unique combination of owner and name within an article table (e.g. shirt)

--- migrations/003-modelsetup.js
+++ migrations/003-modelsetup.js
@@ -2,25 +2,33 @@ const query = require('../modules/query');
 
 queryText = `CREATE TABLE shirt (
 	id SERIAL PRIMARY KEY,
-	name text UNIQUE
+	name text NOT NULL,
+	owner_id integer REFERENCES app_user(id) NOT NULL,
+	UNIQUE (name, owner_id)
 ) INHERITS (article);
 
 CREATE TABLE pants (
 	id SERIAL PRIMARY KEY,
-	name text UNIQUE
+	name text NOT NULL,
+	owner_id integer REFERENCES app_user(id) NOT NULL,
+	UNIQUE (name, owner_id)
 ) INHERITS (article);
 
 CREATE TYPE outerwear_type AS ENUM ('sweater', 'jacket', 'vest', 'raincoat', 'snowcoat');
 
 CREATE TABLE outerwear (
 	id SERIAL PRIMARY KEY,
-	name text UNIQUE,
+	name text NOT NULL,
+	owner_id integer REFERENCES app_user(id) NOT NULL,
+	UNIQUE (name, owner_id),
 	specific_type outerwear_type
 ) INHERITS (article);
 
 CREATE TABLE dress (
 	id SERIAL PRIMARY KEY,
-	name text UNIQUE
+	name text NOT NULL,
+	owner_id integer REFERENCES app_user(id) NOT NULL,
+	UNIQUE (name, owner_id)
 ) INHERITS (article);`;
 
 query(queryText, []);
