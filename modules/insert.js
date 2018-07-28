ef7615eaa68506108a6a97869332eb4a18822b84
henrywoody <github@henrywoody.com>
2018-07-28 00:54:35 -0700

    adds insert module (as in sql INSERT) for articles with tests

---
+++ modules/insert.js
@@ -0,0 +1,41 @@
+const	query = require('./query'),
+		tableIsAllowed = require('./table-is-allowed'),
+		cleanArticleData = require('./clean-article-data'),
+		dataToSQL = require('./data-to-sql-format'),
+		camelCaseKeys = require('./camel-case-keys');
+
+async function intoTableValues (table, data) {
+	if (!tableIsAllowed(table))
+		return null;
+
+	cleanData = cleanArticleData(table, data);
+	
+	const { columns, queryValueSQLVars, queryValues } = dataToSQL(cleanData);
+	const queryText = `INSERT INTO ${table}(${columns}) VALUES(${queryValueSQLVars}) RETURNING *`;
+	const insertResult = await query(queryText, queryValues);
+
+	const { rows } = insertResult;
+	if (!rows) {
+		const { name, constraint, column, routine } = insertResult;
+		if (name === 'error') {
+			let err;
+			if (constraint === 'shirt_name_owner_id_key') {
+				err = new Error('Name is taken.');
+			} else if (column === 'name') {
+				err = new Error('Name cannot be empty.');
+			} else if (constraint === 'article_rating_check') {
+				err = new Error('Rating cannot be below 1 or exceed 5.');
+			} else if (routine === 'enum_in') {
+				err = new Error('Invalid value.');
+			}
+			err.name = 'ValidationError';
+			throw err;
+		}
+	} else {
+		return camelCaseKeys(rows[0]);
+	}
+}
+
+module.exports = {
+	intoTableValues: intoTableValues
+};
---
+++ test/modules/insert.js
@@ -0,0 +1,108 @@
+const	chai = require('chai'),
+		assert = chai.assert,
+		insert = require('../../modules/insert'),
+		query = require('../../modules/query'),
+		createUser = require('../../modules/create-user'),
+		camelCaseKeys = require('../../modules/camel-case-keys');
+
+describe('Insert module', () => {
+	let goodUser, badUser;
+
+	before(async () => {
+		goodUser = await createUser({username: 'gooduser', password: 'goodpass123'});
+		badUser = await createUser({username: 'baduser', password: 'badpass123'});
+	});
+
+	it('should throw a ValidationError if the given article has no name', async () => {
+		try {
+			await insert.intoTableValues('shirt', {ownerId: badUser.id});
+			assert.fail(0, 1, 'Error not thrown');
+		} catch (err) {
+			if (err.name === 'AssertionError')
+				throw err;
+			assert.strictEqual(err.name, 'ValidationError');
+		}
+	});
+
+	it('should throw a ValidationError if there is already an article of the same type with the same name for the same user', async () => {
+		await insert.intoTableValues('shirt', {name: 'Nice Shirt', ownerId: badUser.id});
+		try {
+			await insert.intoTableValues('shirt', {name: 'Nice Shirt', ownerId: badUser.id});
+			assert.fail(0, 1, 'Error not thrown');
+		} catch (err) {
+			if (err.name === 'AssertionError')
+				throw err;
+			assert.strictEqual(err.name, 'ValidationError');
+		}
+	});
+
+	it('should throw a ValidationError if the given rating on the article is above 5 or below 1', async () => {
+		try {
+			await insert.intoTableValues('shirt', {name: 'Nice Shirt', ownerId: badUser.id, rating: 100});
+			assert.fail(0, 1, 'Error not thrown');
+		} catch (err) {
+			if (err.name === 'AssertionError')
+				throw err;
+			assert.strictEqual(err.name, 'ValidationError');
+		}
+
+		try {
+			await insert.intoTableValues('shirt', {name: 'Bad Shirt', ownerId: badUser.id, rating: -10});
+			assert.fail(0, 1, 'Error not thrown');
+		} catch (err) {
+			if (err.name === 'AssertionError')
+				throw err;
+			assert.strictEqual(err.name, 'ValidationError');
+		}
+	});
+
+	it('should throw a ValidationError if the given an invalid specificType for an outerwear', async () => {
+		try {
+			await insert.intoTableValues('outerwear', {name: 'Not a coat', ownerId: badUser.id, specificType: 'somethingElse'});
+			assert.fail(0, 1, 'Error not thrown');
+		} catch (err) {
+			if (err.name === 'AssertionError')
+				throw err;
+			assert.strictEqual(err.name, 'ValidationError');
+		}
+	});
+
+	it('should not throw an error if the given name matches an article of a differnt type for the same owner', async () => {
+		try {
+			await insert.intoTableValues('shirt', {name: 'Favorite', ownerId: goodUser.id});
+			await insert.intoTableValues('pants', {name: 'Favorite', ownerId: goodUser.id});
+		} catch (err) {
+			assert.fail(0, 1, 'Threw an error');
+		}
+	});
+
+	it('should not throw an error if the given name matches an article of a different owner', async () => {
+		try {
+			await insert.intoTableValues('shirt', {name: 'Favorite', ownerId: goodUser.id});
+			await insert.intoTableValues('shirt', {name: 'Favorite', ownerId: badUser.id});
+		} catch (err) {
+			assert.fail(0, 1, 'Threw an error');
+		}
+	});
+
+	it('should return the newly created article', async () => {
+		const coatData = {name: 'Favorite', ownerId: goodUser.id, description: 'the best', rating: 5, color: 'red', maxTemp: 100, minTemp: 70, rainOk: false, snowOk: false, specificType: 'jacket'};
+		const newCoat = await insert.intoTableValues('outerwear', coatData);
+
+		const newCamelCoat = camelCaseKeys(newCoat);
+		console.log(newCamelCoat)
+		for (const key in coatData) {
+			assert.strictEqual(newCamelCoat[key], coatData[key]);
+		}
+	});
+
+	afterEach(async () => {
+		await query('DELETE FROM shirt *');
+		await query('DELETE FROM pants *');
+		await query('DELETE FROM outerwear *');
+	})
+
+	after(async () => {
+		await query('DELETE FROM app_user *');
+	});
+});
