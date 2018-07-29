9104221163af70f42521f0fb7f1fbacb3176ab02
henrywoody <github@henrywoody.com>
2018-07-28 20:16:22 -0700

    creates join module and tests for it

---
+++ modules/join.js
@@ -0,0 +1,89 @@
+const 	query = require('./query'),
+		select = require('./select'),
+		camelCaseKeys = require('./camel-case-keys');
+
+async function tableIdToTableId(table1, id1, table2, id2) {
+	const joinTable = getJoinTable(table1, table2);
+	await checkHaveSameOwner(table1, id1, table2, id2);
+	alreadyJoined = await joinAlreadyExists(joinTable, table1, id1, table2, id2);
+	if (alreadyJoined)
+		return null;
+
+	let queryText, queryValues;
+	switch (joinTable) {
+		case 'shirt_pants_join':
+			queryText = "INSERT INTO shirt_pants_join(shirt_id, pants_id) VALUES ($1, $2) RETURNING *";
+			queryValues = table1 === 'shirt' ? [id1, id2] : [id2, id1];
+			break;
+		case 'shirt_outerwear_join':
+			queryText = "INSERT INTO shirt_outerwear_join(shirt_id, outerwear_id) VALUES ($1, $2) RETURNING *";
+			queryValues = table1 === 'shirt' ? [id1, id2] : [id2, id1];
+			break;
+		case 'pants_outerwear_join':
+			queryText = "INSERT INTO pants_outerwear_join(pants_id, outerwear_id) VALUES ($1, $2) RETURNING *";
+			queryValues = table1 === 'pants' ? [id1, id2] : [id2, id1];
+			break;
+		case 'outerwear_outerwear_join':
+			queryText = "INSERT INTO outerwear_outerwear_join(a_outerwear_id, b_outerwear_id) VALUES ($1, $2) RETURNING *";
+			queryValues = [id1, id2];
+			break;
+		case 'dress_outerwear_join':
+			queryText = "INSERT INTO dress_outerwear_join(dress_id, outerwear_id) VALUES ($1, $2) RETURNING *";
+			queryValues = table1 === 'dress' ? [id1, id2] : [id2, id1];
+			break;
+	}
+	
+	const { rows } = await query(queryText, queryValues);
+	return camelCaseKeys(rows[0]);
+}
+
+function getJoinTable(...tablePair) {
+	const validJoins = [
+		['pants', 'shirt', 'shirt_pants_join'], ['outerwear', 'shirt', 'shirt_outerwear_join'],
+		['outerwear', 'pants', 'pants_outerwear_join'], ['outerwear', 'outerwear', 'outerwear_outerwear_join'],
+		['dress', 'outerwear', 'dress_outerwear_join']
+	];
+
+	for (const validJoin of validJoins) {
+		if (validJoin[0] === tablePair[0] && validJoin[1] === tablePair[1] || validJoin[0] === tablePair[1] && validJoin[1] === tablePair[0])
+			return validJoin[2];
+	}
+	const err = new Error('Table pair is not valid for join');
+	err.name = 'ValidationError';
+	throw err;
+}
+
+async function checkHaveSameOwner(table1, id1, table2, id2) {
+	const object1 = await select.fromTableById(table1, id1);
+	const object2 = await select.fromTableById(table2, id2);
+
+	if (!object1 || !object2) {
+		const err = new Error('Article not found');
+		err.name = 'NotFound';
+		throw err;
+	}
+
+	if (object1.ownerId !== object2.ownerId) {
+		const err = new Error('Articles are not owned by the same owner');
+		err.name = 'ValidationError';
+		throw err;
+	}
+}
+
+async function joinAlreadyExists(joinTable, column1, id1, column2, id2) {
+	let queryText;
+	if (joinTable === 'outerwear_outerwear_join') {
+		queryText = "SELECT * FROM outerwear_outerwear_join WHERE a_outerwear_id = $1 AND b_outerwear_id = $2 OR a_outerwear_id = $2 AND b_outerwear_id = $1";
+	} else {
+		queryText = `SELECT * FROM ${joinTable} WHERE ${column1}_id = $1 AND ${column2}_id = $2`;
+	}
+	const queryValues = [id1, id2];
+	const { rows } = await query(queryText, queryValues);
+	if (rows.length)
+		return true;
+	return false;
+}
+
+module.exports = {
+	tableIdToTableId: tableIdToTableId
+}
---
+++ test/modules/join.js
@@ -0,0 +1,138 @@
+const	chai = require('chai'),
+		assert = chai.assert,
+		join = require('../../modules/join'),
+		insert = require('../../modules/insert'),
+		query = require('../../modules/query'),
+		createUser = require('../../modules/create-user');
+
+
+describe ('Join module', () => {
+	let goodUser, badUser;
+
+	before(async () => {
+		goodUser = await createUser({username: 'goodUser', password: 'password'});
+		badUser = await createUser({username: 'badUser', password: 'password'});
+	});
+
+	describe('tableIdToTableId method', () => {
+		let shirt, pants, jacket, sweater, dress, badPants;
+
+		beforeEach(async () => {
+			shirt = await insert.intoTableValues('shirt', {name: 'good shirt', ownerId: goodUser.id});
+			pants = await insert.intoTableValues('pants', {name: 'good pants', ownerId: goodUser.id});
+			jacket = await insert.intoTableValues('outerwear', {name: 'good jacket', ownerId: goodUser.id});
+			sweater = await insert.intoTableValues('outerwear', {name: 'good sweater', ownerId: goodUser.id});
+			dress = await insert.intoTableValues('dress', {name: 'good dress', ownerId: goodUser.id});
+			badPants = await insert.intoTableValues('pants', {name: 'bad pants', ownerId: badUser.id});
+		});
+
+		it('should throw an ValidationError if table combination is not valid', async () => {
+			try {
+				await join.tableIdToTableId('dress', dress.id, 'pants', pants.id);
+				assert.fail(0, 1, 'No error was thrown');
+			} catch (err) {
+				if (err.name === 'AssertionError')
+					throw err;
+				assert.strictEqual(err.name, 'ValidationError');
+			}
+		});
+
+		it('should throw an ValidationError and not create a join between two objects owned by different users', async () => {
+			try {
+				await join.tableIdToTableId('shirt', shirt.id, 'pants', badPants.id);
+				assert.fail(0, 1, 'No error was thrown');
+			} catch (err) {
+				if (err.name === 'AssertionError')
+					throw err;
+				assert.strictEqual(err.name, 'ValidationError');
+			}
+		});
+
+		it('should throw a NotFound error if either of the object ids is invalid', async () => {
+			try {
+				await join.tableIdToTableId('shirt', shirt.id + 100, 'pants', badPants.id);
+				assert.fail(0, 1, 'No error was thrown');
+			} catch (err) {
+				if (err.name === 'AssertionError')
+					throw err;
+				assert.strictEqual(err.name, 'NotFound');
+			}
+		});
+
+		it('should create a join between a valid pair of objects and return the join with camelCased keys', async () => {
+			let joinObj;
+
+			joinObj = await join.tableIdToTableId('shirt', shirt.id, 'pants', pants.id);
+			assert.strictEqual(joinObj.shirtId, shirt.id);
+			assert.strictEqual(joinObj.pantsId, pants.id);
+
+			joinObj = await join.tableIdToTableId('shirt', shirt.id, 'outerwear', jacket.id);
+			assert.strictEqual(joinObj.shirtId, shirt.id);
+			assert.strictEqual(joinObj.outerwearId, jacket.id);
+
+			joinObj = await join.tableIdToTableId('pants', pants.id, 'outerwear', jacket.id);
+			assert.strictEqual(joinObj.pantsId, pants.id);
+			assert.strictEqual(joinObj.outerwearId, jacket.id);
+
+			joinObj = await join.tableIdToTableId('outerwear', jacket.id, 'outerwear', sweater.id);
+			assert.strictEqual(joinObj.aOuterwearId, jacket.id);
+			assert.strictEqual(joinObj.bOuterwearId, sweater.id);
+
+			joinObj = await join.tableIdToTableId('dress', dress.id, 'outerwear', sweater.id);
+			assert.strictEqual(joinObj.dressId, dress.id);
+			assert.strictEqual(joinObj.outerwearId, sweater.id);
+		});
+
+		it('should create a join between a valid pair of objects *regardless of the order given* and return the join with camelCased keys', async () => {
+			let joinObj;
+
+			joinObj = await join.tableIdToTableId('pants', pants.id, 'shirt', shirt.id);
+			assert.strictEqual(joinObj.shirtId, shirt.id);
+			assert.strictEqual(joinObj.pantsId, pants.id);
+			
+			joinObj = await join.tableIdToTableId('outerwear', jacket.id, 'shirt', shirt.id);
+			assert.strictEqual(joinObj.shirtId, shirt.id);
+			assert.strictEqual(joinObj.outerwearId, jacket.id);
+			
+			joinObj = await join.tableIdToTableId('outerwear', jacket.id, 'pants', pants.id);
+			assert.strictEqual(joinObj.pantsId, pants.id);
+			assert.strictEqual(joinObj.outerwearId, jacket.id);
+			
+			joinObj = await join.tableIdToTableId('outerwear', sweater.id, 'outerwear', jacket.id);
+			assert.strictEqual(joinObj.aOuterwearId, sweater.id);
+			assert.strictEqual(joinObj.bOuterwearId, jacket.id);
+			
+			joinObj = await join.tableIdToTableId('outerwear', sweater.id, 'dress', dress.id);
+			assert.strictEqual(joinObj.dressId, dress.id);
+			assert.strictEqual(joinObj.outerwearId, sweater.id);
+		});
+
+		it('should not throw an error but return null if asked to create a join that already exists', async () => {
+			await join.tableIdToTableId('pants', pants.id, 'shirt', shirt.id);
+			const joinObj = await join.tableIdToTableId('pants', pants.id, 'shirt', shirt.id);
+
+			assert.isNull(joinObj);
+		});
+
+		afterEach(async () => {
+			// NOTE: these need to be two separate Promise.alls because joins must be deleted before articles for CASCADING in SQL
+			await Promise.all([
+				query("DELETE FROM dress_outerwear_join *"),
+				query("DELETE FROM outerwear_outerwear_join *"),
+				query("DELETE FROM pants_outerwear_join *"),
+				query("DELETE FROM shirt_outerwear_join *"),
+				query("DELETE FROM shirt_pants_join *")
+			]);
+			await Promise.all([
+				query("DELETE FROM shirt *"),
+				query("DELETE FROM pants *"),
+				query("DELETE FROM outerwear *"),
+				query("DELETE FROM dress *")
+			]);
+		});
+	});
+
+	after(async () => {
+		await query("DELETE FROM app_user *");
+	});
+});
