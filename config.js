8d1565c4d16c920c6eee43c4cc2b2634698659dc
henrywoody <github@henrywoody.com>
2018-09-01 19:18:24 -0700

    adds config for production

--- config.js
+++ config.js
@@ -26,6 +26,19 @@ config.test = {
 	awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 };
 
+// Production
+config.prod = {
+	env: 'prod',
+	dbName: process.env.PROD_DB_NAME,
+	dbHost: process.env.PROD_DB_HOST,
+	dbUser: process.env.PROD_DB_USER,
+	dbPassword: process.env.PROD_DB_PASSWORD,
+	appSecret: process.env.PROD_APP_SECRET,
+	imagesBucket: process.env.PROD_IMAGES_BUCKET,
+	awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+}
+
 
 
 module.exports = config;
