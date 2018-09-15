01efc358f6377161435383cdf748650ea416ea3d
henrywoody <github@henrywoody.com>
2018-09-15 13:59:47 -0700

    adds deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+ACTIVE_PROJECT=$(gcloud config list | grep -e 'project\s=\s.*')
+if [[ $ACTIVE_PROJECT != "project = drobe-1529715129105" ]]
+then
+	echo "Switching Google Cloud Project to 'drobe-1529715129105'."
+	gcloud config set project drobe-1529715129105
+fi
+
+npm run-script build --prefix ./client
+gcloud app deploy --quiet
